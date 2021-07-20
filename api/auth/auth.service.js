0a0b9a7b1321b894043db1280dae0cafafe80067
Ilya Grigoriev Starr <ilgrist67@gmail.com>
2021-07-20 14:23:59 +0300

    minor changes in user service

--- api/auth/auth.service.js
+++ api/auth/auth.service.js
@@ -3,7 +3,6 @@ const userService = require('../user/user.service');
 // const logger = require('../../services/logger.service');
 
 async function login(username, password) {
-
   const user = await userService.getByUsername(username);
   if (!user) return Promise.reject('Invalid username or password');
   const match = await bcrypt.compare(password, user.password);
