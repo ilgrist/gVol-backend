1442112a78e36d8490e312ce918b14e753a6f810
Ilya Grigoriev Starr <ilgrist67@gmail.com>
2021-07-22 18:58:22 +0300

    added socket support, emitToAll func

--- services/socket.service.js
+++ services/socket.service.js
@@ -1,80 +1,105 @@
-
-
 const asyncLocalStorage = require('./als.service');
 const logger = require('./logger.service');
 
-var gIo = null
-var gSocketBySessionIdMap = {}
+var gIo = null;
 
 function connectSockets(http, session) {
-    gIo = require('socket.io')(http);
-
-    const sharedSession = require('express-socket.io-session');
-
-    gIo.use(sharedSession(session, {
-        autoSave: true
-    }));
-    gIo.on('connection', socket => {
-        console.log('New socket - socket.handshake.sessionID', socket.handshake.sessionID)
-        gSocketBySessionIdMap[socket.handshake.sessionID] = socket
-        // TODO: emitToUser feature - need to tested for CaJan21
-        // if (socket.handshake?.session?.user) socket.join(socket.handshake.session.user._id)
-        socket.on('disconnect', socket => {
-            console.log('Someone disconnected')
-            if (socket.handshake) {
-                gSocketBySessionIdMap[socket.handshake.sessionID] = null
-            }
-        })
-        socket.on('chat topic', topic => {
-            if (socket.myTopic === topic) return;
-            if (socket.myTopic) {
-                socket.leave(socket.myTopic)
-            }
-            socket.join(topic)
-            // logger.debug('Session ID is', socket.handshake.sessionID)
-            socket.myTopic = topic
-        })
-        socket.on('chat newMsg', msg => {
-            // emits to all sockets:
-            // gIo.emit('chat addMsg', msg)
-            // emits only to sockets in the same room
-            gIo.to(socket.myTopic).emit('chat addMsg', msg)
-        })
-        socket.on('user-watch', userId => {
-            socket.join(userId)
-        })
-
-    })
+  gIo = require('socket.io')(http);
+  gIo.on('connection', (socket) => {
+    console.log('New socket', socket.id);
+    socket.on('disconnect', (socket) => {
+      console.log('Someone disconnected');
+    });
+    socket.on('chat topic', (topic) => {
+      if (socket.myTopic === topic) return;
+      if (socket.myTopic) {
+        socket.leave(socket.myTopic);
+      }
+      socket.join(topic);
+      socket.myTopic = topic;
+    });
+    socket.on('chat newMsg', (msg) => {
+      // emits to all sockets:
+      // gIo.emit('chat addMsg', msg)
+      // emits only to sockets in the same room
+      gIo.to(socket.myTopic).emit('chat addMsg', msg);
+    });
+    socket.on('user-watch', (userId) => {
+      socket.join('watching:' + userId);
+    });
+    socket.on('isTyping', (fullname) => {
+      console.log('isTyping:', fullname);
+      gIo.to(socket.myTopic).emit('getTyping', fullname);
+    });
+    socket.on('set-user-socket', (userId) => {
+      logger.debug(`Setting socket.userId = ${userId}`);
+      socket.userId = userId;
+    });
+    socket.on('unset-user-socket', () => {
+      delete socket.userId;
+    });
+  });
 }
 
-function emitToAll({ type, data, room = null }) {
-    if (room) gIo.to(room).emit(type, data)
-    else gIo.emit(type, data)
+function emitToAll(type, data) {
+  gIo.emit(type, data);
 }
 
-// TODO: Need to test emitToUser feature
+function emitTo({ type, data, label }) {
+  if (label) gIo.to('watching:' + label).emit(type, data);
+  else gIo.emit(type, data);
+}
+
 function emitToUser({ type, data, userId }) {
-    gIo.to(userId).emit(type, data)
+  logger.debug('Emiting to user socket: ' + userId);
+  const socket = _getUserSocket(userId);
+  if (socket) socket.emit(type, data);
+  else {
+    console.log('User socket not found');
+    _printSockets();
+  }
 }
 
-
-// Send to all sockets BUT not the current socket 
-function broadcast({ type, data, room = null }) {
-    const store = asyncLocalStorage.getStore()
-    const { sessionId } = store
-    if (!sessionId) return logger.debug('Shoudnt happen, no sessionId in asyncLocalStorage store')
-    const excludedSocket = gSocketBySessionIdMap[sessionId]
-    if (!excludedSocket) return logger.debug('Shouldnt happen, No socket in map')
-    if (room) excludedSocket.broadcast.to(room).emit(type, data)
-    else excludedSocket.broadcast.emit(type, data)
+// Send to all sockets BUT not the current socket
+function broadcast({ type, data, room = null, userId }) {
+  const excludedSocket = _getUserSocket(userId);
+  if (!excludedSocket) {
+    logger.debug('Shouldnt happen, socket not found');
+    _printSockets();
+    return;
+  }
+  logger.debug('broadcast to all but user: ', userId);
+  if (room) {
+    excludedSocket.broadcast.to(room).emit(type, data);
+  } else {
+    excludedSocket.broadcast.emit(type, data);
+  }
 }
 
+function _getUserSocket(userId) {
+  const sockets = _getAllSockets();
+  const socket = sockets.find((s) => s.userId == userId);
+  return socket;
+}
+function _getAllSockets() {
+  const socketIds = Object.keys(gIo.sockets.sockets);
+  const sockets = socketIds.map((socketId) => gIo.sockets.sockets[socketId]);
+  return sockets;
+}
+
+function _printSockets() {
+  const sockets = _getAllSockets();
+  console.log(`Sockets: (count: ${sockets.length}):`);
+  sockets.forEach(_printSocket);
+}
+function _printSocket(socket) {
+  console.log(`Socket - socketId: ${socket.id} userId: ${socket.userId}`);
+}
 
 module.exports = {
-    connectSockets,
-    emitToAll,
-    broadcast,
-}
-
-
-
+  connectSockets,
+  emitTo,
+  emitToUser,
+  broadcast,
+  emitToAll,
+};
